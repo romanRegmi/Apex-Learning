acaecdc7deb0015c7d2221028abb08611c9985c9
Roman Regmi <romanregmi@hotmail.com>
2023-12-09 20:17:40 +0545

    Added Enum Class

---
+++ Apex/Enum/EnumClass.java
@@ -0,0 +1,11 @@
+/*
+ * Enum is a user defined data type. 
+ * Values other than that defined in the enum will not be accepted.
+*/
+
+public class EnumClass {
+    public static void myMethod() {
+        Season s = Season.Winter;
+        s = Season.Fall;
+    }
+}
