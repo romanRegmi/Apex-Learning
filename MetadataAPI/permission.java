24c61517d8c63a779a2ec6860c47f25b05d8a901
Roman Regmi <20965507+romanRegmi@users.noreply.github.com>
2024-12-24 09:16:26 +0545

    metadata update

--- MetadataAPI/permission.java
+++ MetadataAPI/permission.java
@@ -1,40 +1,78 @@
-// Create a new permission set
-PermissionSet ps = new PermissionSet();
-ps.Name = 'Account_Fields';
-ps.Label = 'Account Fields Permission Set';
+public class createPS {
+    
+    public static String definePS(String Name, String Label){
+        
+        PermissionSet ps = new PermissionSet();
+        ps.Name = Name;
+        ps.Label = Label;
+        
+        insert ps;
+        
+        return ps.Id;
+    }
+    
+    
+    public static void defineObjectPS(String objAPIName, Id psId) {
+        ObjectPermissions objPs = new ObjectPermissions();
+        objPs.ParentId = psId;
+        objPs.SobjectType = objAPIName;
+        objPs.PermissionsRead = true;
+        objPs.PermissionsCreate = true;
+        objPs.PermissionsEdit = true;
+        objPs.PermissionsDelete = true;
+        objPs.PermissionsViewAllRecords = true;
+        objPs.PermissionsModifyAllRecords = true;
+        
+        // Insert the object permissions
+        insert objPs;    
+    }
+    
+    
+    public static void defineFieldPermissions(String objectApiName, Id permissionSetId) {
+        // List to hold the FieldPermissions records to be inserted
+        List<FieldPermissions> fieldPermissionsList = new List<FieldPermissions>();
+        
+        // Retrieve the SObject type using the objectApiName
+        Schema.SObjectType sObjectType = Schema.getGlobalDescribe().get(objectApiName);
+        
+        // Get the field map for the SObject
+        Schema.DescribeSObjectResult describeResult = sObjectType.getDescribe();
+        Map<String, Schema.SObjectField> fieldMap = describeResult.fields.getMap();
+        
+        
+        // Iterate through all the fields of the SObject
+        for (String fieldName : fieldMap.keySet()) {
+            // Create a new FieldPermissions record for each field
+            FieldPermissions fieldPermission = new FieldPermissions();
+            fieldPermission.ParentId = permissionSetId;  // Set the PermissionSet Id
+            fieldPermission.SobjectType =  objectApiName; // Set the Object API Name
+            fieldPermission.Field = objectApiName + '.' + fieldName;           // Set the Field Name
+            fieldPermission.PermissionsRead = true;      // Grant read access
+            fieldPermission.PermissionsEdit = true;      // Grant edit access
+            
+            // Add the FieldPermissions record to the list
+            fieldPermissionsList.add(fieldPermission);
+        } 
+        
+        //System.assert(false, fieldPermissionsList);
+        
+        // Insert the FieldPermissions records in bulk
+        if (!fieldPermissionsList.isEmpty()) {
+            Database.insert(fieldPermissionsList, false);
+        }
+    }
 
-// Insert the permission set
-insert ps;
 
-// Define the object permissions for the Account object
-ObjectPermissions accountObjPermission = new ObjectPermissions();
-accountObjPermission.ParentId = ps.Id;
-accountObjPermission.SobjectType = 'Account';
-accountObjPermission.PermissionsRead = true;
-accountObjPermission.PermissionsCreate = true;
-accountObjPermission.PermissionsEdit = true;
-accountObjPermission.PermissionsDelete = true;
-accountObjPermission.PermissionsViewAllRecords = true;
-accountObjPermission.PermissionsModifyAllRecords = true;
+    public static void assignPSToCurrentUser(Id psId){
+        // Assign the permission set to a user
+        PermissionSetAssignment psa = new PermissionSetAssignment();
+        psa.AssigneeId = UserInfo.getUserId();
+        psa.PermissionSetId = psId;
+        
+        // Insert the permission set assignment
+        insert psa;
+    }    
+    
+    
 
-// Insert the object permissions
-insert accountObjPermission;
-
-// Define the field permissions for the Name field
-FieldPermissions nameFieldPermission = new FieldPermissions();
-nameFieldPermission.ParentId = ps.Id;
-nameFieldPermission.SobjectType = 'Account';
-nameFieldPermission.Field = 'Name';
-nameFieldPermission.PermissionsRead = true;
-nameFieldPermission.PermissionsEdit = true;
-
-// Insert the field permissions
-insert nameFieldPermission;
-
-// Assign the permission set to a user
-PermissionSetAssignment psa = new PermissionSetAssignment();
-psa.AssigneeId = UserInfo.getUserId();
-psa.PermissionSetId = ps.Id;
-
-// Insert the permission set assignment
-insert psa;
+}
--- MetadataAPI/tabs.java
+++ MetadataAPI/tabs.java
@@ -68,7 +68,7 @@ private static void handleSaveResults(MetadataService.SaveResult saveResult) {
                     'ields ' String.join(error.fields, ',') + '.' : '' ) );
         }
         if (messages.size() > 0) {
-            throw new MetadataServiceException(String.join(messages, '));
+            throw new MetadataServiceException(String.join(messages, ''));
         }
     }
     if (!saveResult.success) {
@@ -78,3 +78,65 @@ private static void handleSaveResults(MetadataService.SaveResult saveResult) {
 
 // Custom exception class
 public class MetadataServiceException extends Exception { }
+
+
+
+
+public class tabCreation {
+    
+    public static Boolean doesTabExist(String objectAPIName){
+        // Get the current list of tabs
+        List<Schema.DescribeTabSetResult> tabSetDesc = Schema.describeTabs();
+        
+        
+        boolean tabExists = false;
+        // Iterate through the tab sets
+        for (DescribeTabSetResult tsr : tabSetDesc) {
+            // Get the tabs for the current tab set
+            List<Schema.DescribeTabResult> tabDesc = tsr.getTabs();
+        
+            // Check if the custom object tab already exists
+            for (DescribeTabResult tr : tabDesc) {
+                if (tr.getSobjectName() == objectAPIName) {
+                    tabExists = true;
+                }
+            }
+        }
+        
+        return tabExists;
+        
+        
+    }
+    
+    
+    public static void createTab(String objectAPIName){
+        MetadataService.CustomTab customTab = new MetadataService.CustomTab();
+        customTab.fullName = objectAPIName;
+        customTab.label = objectAPIName.removeEnd('__c').replace('_', ' ');
+        customTab.mobileReady = true;
+        customTab.customObject = true;
+        customTab.motif = 'Custom20:Airplane';
+        
+        // Add the custom tab to the list of tabs
+        MetadataService.MetadataPort service = createService();
+        try{
+        List<MetadataService.SaveResult> results =
+            service.createMetadata(
+                new MetadataService.Metadata[] { customTab });
+            
+            //System.assert(false, results);
+        } catch(Exception ex){
+            System.assert(false, ex);
+        }
+    }
+    
+    // Method to create a MetadataService instance
+    private static MetadataService.MetadataPort createService() {
+        MetadataService.MetadataPort service = new MetadataService.MetadataPort();
+        service.SessionHeader = new MetadataService.SessionHeader_element();
+        service.SessionHeader.sessionId = UserInfo.getSessionId();
+        return service;
+    }
+    
+
+}
